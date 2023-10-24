ba3097c61afcdafc12fb433a4b34aaaea9a27c1b
BerdinaKatya <kuznkate42@gmail.com>
2023-10-24 12:20:13 +0500

    almost final

--- SliderGame/ContentView.swift
+++ SliderGame/ContentView.swift
@@ -8,14 +8,54 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var sliderValue = Float.random(in: 0...100)
+    @State private var targetValue = Int.random(in: 0...100)
+    @State private var alphaValue = 1.0
+    @State private var isAlertPresented = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Подвиньте слайдер как можно ближе к: \(targetValue)")
+            
+            HStack {
+                Text("0")
+                
+                UISliderRepresentation(
+                    sliderValue: $sliderValue,
+                    targetValue: $targetValue,
+                    alphaValue: $alphaValue
+                )
+                
+                Text("100")
+            }.padding(.top, 20)
+            
+            Button(action: {isAlertPresented.toggle()}, label: {
+                Text("Проверь меня!")
+            })
+            .padding(.top, 10)
+            .alert(
+                "Ваш результат",
+                isPresented: $isAlertPresented,
+                actions: {}) {
+                    Text("\(computeScore())")
+                }
+            
+            Button(
+                action: {
+                    targetValue = Int.random(in: 0...100)
+                    sliderValue = Float.random(in: 0...100)
+                },
+                label: {
+                Text("Начать заново")
+            })
+            .padding(.top, 8)
         }
         .padding()
+    }
+    
+    private func computeScore() -> Int {
+        let difference = abs(targetValue - lround(Double(sliderValue)))
+        return 100 - difference
     }
 }
 
---
+++ SliderGame/UISliderRepresentation.swift
@@ -0,0 +1,75 @@
+//
+//  SliderRepresentation.swift
+//  SliderGame
+//
+//  Created by Ekaterina Teliauberdina on 24.10.2023.
+//
+
+import SwiftUI
+
+struct UISliderRepresentation: UIViewRepresentable {
+
+    @Binding var sliderValue: Float
+    @Binding var targetValue: Int
+    @Binding var alphaValue: Double
+    
+    func makeUIView(context: Context) -> UISlider {
+        let slider = UISlider()
+        
+        slider.minimumValue = 0
+        slider.maximumValue = 100
+        slider.value = sliderValue
+        slider.thumbTintColor = .red
+        
+        slider.addTarget(
+            context.coordinator,
+            action: #selector(Coordinator.sliderValueChanged),
+            for: .valueChanged
+        )
+        
+        return slider
+    }
+    
+    func updateUIView(_ uiView: UISlider, context: Context) {
+        uiView.value = sliderValue
+        uiView.thumbTintColor = UIColor(
+            red: 255 / 255,
+            green: 1 / 255,
+            blue: 1 / 255,
+            alpha: CGFloat(alphaValue)
+        )
+    }
+    
+//    func getAlphaValue(sliderValue: Float, targetNumber: Int) -> CGFloat {
+//        var result: CGFloat = 0
+//        
+//        if sliderValue == Float(targetNumber) {
+//            result = 1
+//        } else if sliderValue > Float(targetNumber) {
+//            result = CGFloat(1 / (sliderValue / Float(targetNumber)))
+//        } else if sliderValue < Float(targetNumber) {
+//            result = CGFloat(0.4 + sliderValue / Float(targetNumber))
+//        }
+//        
+//        return result
+//    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(sliderValue: $sliderValue)
+    }
+    
+}
+
+extension UISliderRepresentation {
+    final class Coordinator: NSObject {
+        @Binding var sliderValue: Float
+        
+        init(sliderValue: Binding<Float>) {
+            self._sliderValue = sliderValue
+        }
+        
+        @objc func sliderValueChanged(_ sender: UISlider) {
+            sliderValue = sender.value
+        }
+    }
+}
