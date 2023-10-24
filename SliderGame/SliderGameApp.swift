45376d329d73e3de477829f33f189a0b8594175b
BerdinaKatya <kuznkate42@gmail.com>
2023-10-24 08:58:14 +0500

    Initial Commit

---
+++ SliderGame/SliderGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderGameApp.swift
+//  SliderGame
+//
+//  Created by Ekaterina Teliauberdina on 24.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct SliderGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
